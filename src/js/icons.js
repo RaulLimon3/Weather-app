10800bae7f04757cd3eda1847c10423ae2493b4d
Raul <raul_l1m0n@hotmail.com>
2026-08-22 16:00:22 -0600

    feat: style app layout and search bar

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,10 +1,23 @@
 // Importamos los inconos a utilizar
-import { createIcons, Search, X, Plus } from "lucide";
+import {
+    createIcons, Search, X, Plus, ThermometerSun, Sun, SunDim,
+    WindArrowDown, Droplets, Cloudy, Wind, Sunrise, Sunset, Eye
+} from "lucide";
 
 createIcons({
     icons: {
         Search,
         X,
-        Plus
+        Plus,
+        ThermometerSun,
+        Sun,
+        SunDim,
+        WindArrowDown,
+        Droplets,
+        Cloudy,
+        Wind,
+        Sunrise,
+        Sunset,
+        Eye
     }
 });
