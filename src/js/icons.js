d0b072847700bbf502917f5cca9268b56f960f44
Raul <raul_l1m0n@hotmail.com>
2026-08-24 12:29:54 -0600

    feat: style cards, sections and Lucide icons

--- src/js/icons.js
+++ src/js/icons.js
@@ -3,6 +3,7 @@ import {
     createIcons, Search, X, Plus, ThermometerSun, Sun, SunDim,
     WindArrowDown, Droplets, Cloudy, Wind, Sunrise, Sunset, Eye
 } from "lucide";
+import "../css/components/icon.css"
 
 createIcons({
     icons: {
